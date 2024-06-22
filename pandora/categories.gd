921eda9885c5c6bfa5cb7feb529e0932dc30cc13
miguel <miguel-gonzalez@gmx.de>
2024-06-22 08:18:34 +0100

    upgrade to gdunit to 4.3 (#178)

---
+++ pandora/categories.gd
@@ -0,0 +1,12 @@
+# Do not modify! Auto-generated file.
+class_name PandoraCategories
+
+
+const ROOT = "m0LT2ut_BO"
+
+
+class RootCategories:
+	const ITEMS = "dhuQ_I1owC"
+	const NPCS = "8-oXvCMZmH"
+
+
